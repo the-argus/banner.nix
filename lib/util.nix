708e622f0d99ceb4f6d4c81295bb9becee9c4bf3
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-11-08 20:09:36 -0500

    add makeBase16

--- lib/util.nix
+++ lib/util.nix
@@ -4,4 +4,31 @@
       "author"
       "scheme"
     ];
+  makeBase16 = bannerPalette:
+    builtins.removeAttrs bannerPalette [
+      "highlight"
+      "hialt0"
+      "hialt1"
+      "hialt2"
+      "warn"
+      "confirm"
+      "urgent"
+      "link"
+      "pfg-highlight"
+      "pfg-hialt0"
+      "pfg-hialt1"
+      "pfg-hialt2"
+      "pfg-warn"
+      "pfg-confirm"
+      "pfg-urgent"
+      "pfg-link"
+      "ansi00"
+      "ansi01"
+      "ansi02"
+      "ansi03"
+      "ansi04"
+      "ansi05"
+      "ansi06"
+      "ansi07"
+    ];
 }
